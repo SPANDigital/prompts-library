e37135f73f00d7fa3b6e87827458bed56dd07f56
Richard Wooding <richard.wooding@spandigital.com>
2023-05-30 14:44:35 +0200

    Test data layer and reporting

---
+++ .template-data-layer-and-scripting/_index.md
@@ -0,0 +1,1 @@
+Test use of data layer
---
+++ .template-data-layer-and-scripting/test.js
@@ -0,0 +1,18 @@
+function joinTerms(terms) {
+    if (terms) {
+        var ret = ""
+        for (var i = 0; i<terms.length; i++) {
+            if (i > 0) {
+                ret = ret + ", ";
+                if (i == terms.length -1) {
+                    ret = ret + "and "
+                }
+            }
+            ret = ret + terms[i]
+        }
+        return ret
+    }
+    return ""
+}
+
+addPrompt("user", "What is " + joinTerms(data.animals) + "?")
